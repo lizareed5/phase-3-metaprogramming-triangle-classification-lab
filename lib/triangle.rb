88b63ae43ee00ac0c3d796de23ddc218d8066744
Liza McLain <lizamclain@gmail.com>
2023-01-31 21:27:32 -0600

    commit tree 59c59d3e05860325a0548b76337709134ba8c5fc

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize (a, b, c)
+    self.a = a
+    self.b = b
+    self.c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    a > 0 && b > 0 && c > 0
+    # [a, b, c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
